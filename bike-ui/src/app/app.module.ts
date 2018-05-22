edcb317f8c72ecf7b15843788b7283983d7cbeef
Nacim KACEL <nkacel@finaxys.com>
2018-05-23 00:42:20 +0200

    [feat] Adding reactive form to app.module.ts

--- bike-ui/src/app/app.module.ts
+++ bike-ui/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BikeService } from './services/bike.service';
 import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
